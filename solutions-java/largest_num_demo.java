b07fd4b9816f9aaae291da0ff9c29b57f375f52d
Nidhi2499 <niti19is@cmrit.ac.in>
2024-05-09 08:09:16 +0530

    check

---
+++ solutions-java/largest_num_demo.java
@@ -0,0 +1,6 @@
+// int[] arr = [1, 2, 4, 3,5];
+// int lar = arr[0];
+// for(int i=1; i<arr.length ; i++){
+//     if (arr[i] > lar) lar = arr[i];
+// }
+// System.out.println('The largest number in array is ', lar)
